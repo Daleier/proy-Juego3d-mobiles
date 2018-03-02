fa45467ff9831df5aa4647efe215ac818d0c6187
daleier <davidjavierbp@gmail.com>
2018-03-02 10:34:43 +0100

    Añadido fondo de pantalla y titulo.

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import game.Game;
@@ -7,6 +8,9 @@ import game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Space Defender";
+		config.resizable=false;
+		config.addIcon("icon/ic_asteroid.png", Files.FileType.Internal);
 		new LwjglApplication(new Game(), config);
 	}
 }
